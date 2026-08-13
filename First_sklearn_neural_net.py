8733212951849c0de5f6d6d7d8931189e2793b95
fateme <fatemeepourmand@gmail.com>
2026-08-13 18:22:38 +0330

    Update First sklearn neural networ

--- First_sklearn_neural_net.py
+++ First_sklearn_neural_net.py
@@ -1,13 +1,44 @@
 from sklearn.datasets import load_breast_cancer
+from sklearn.model_selection import train_test_split
+from sklearn.pipeline import Pipeline
+from sklearn.preprocessing import StandardScaler
+from sklearn.neural_network import MLPClassifier
+from sklearn.metrics import accuracy_score
+
 
 data = load_breast_cancer()
 
-x = data.data
+X = data.data
 y = data.target
-print(x.shape)
+
+print(X.shape)
 print(y.shape)
- 
 
-print(data.data.shape)
-print(data.target.shape)
 
+X_train, X_test, y_train, y_test = train_test_split(
+X,
+y,
+test_size=0.2,
+random_state=42
+)
+
+
+pipe = Pipeline([
+("scaler", StandardScaler()),
+("model", MLPClassifier(
+hidden_layer_sizes=(100,),
+max_iter=1000,
+random_state=42
+))
+])
+
+
+pipe.fit(X_train, y_train)
+
+
+y_pred = pipe.predict(X_test)
+
+
+accuracy = accuracy_score(y_test, y_pred)
+
+print("Accuracy:", accuracy)
